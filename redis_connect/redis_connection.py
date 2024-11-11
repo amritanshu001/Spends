5b524966f64f9a919df93b34057048d439d6b3b6
Amritanshu <91420206+amritanshu001@users.noreply.github.com>
2024-11-11 15:24:12 +0530

    adding Render Redis for blocked JWT

--- redis_connect/redis_connection.py
+++ redis_connect/redis_connection.py
@@ -10,7 +10,11 @@ redis_db["host"] = os.getenv("REDIS_HOST")
 redis_db["port"] = os.getenv("REDIS_PORT")
 redis_db["password"] = os.getenv("REDIS_PASSWORD")
 
-
-blocklist_connection = redis.Redis(
-    host=redis_db["host"], port=redis_db["port"], password=redis_db["password"]
-)
+if os.getenv("ENVIRONMENT") == "TEST":
+    blocklist_connection = redis.Redis(
+        host=redis_db["host"],
+        port=redis_db["port"],
+        password=redis_db["password"],
+    )
+else:
+    blocklist_connection = redis.from_url(os.getenv("REDIS_URL"))
